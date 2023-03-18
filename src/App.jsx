98d4bf9d900f7320eec936e27fa7c6e3c94a5c80
unknown <dineshigdd@gmail.com>
2023-03-18 01:20:12 -0700

    add basename to Router

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function PortfolioContainer() {
 
   return (
     <Container fluid>
-       <Router>
+       <Router basename>
           <Row className='d-flex flex-column'>     
             <Col>
               <Navbar bg="light" expand="lg" fixed="top">
