72bd628fd550db354e9e209eab666e769b3ecfb4
unknown <dineshigdd@gmail.com>
2023-03-19 02:28:05 -0700

    mode

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // mode: "development",
+  mode: "development",
   build: {
     minify: false,
   },
