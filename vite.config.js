8c253c1fbf4920428e2734e1e3127bc22a191567
unknown <dineshigdd@gmail.com>
2023-03-19 02:34:58 -0700

    change mdoe

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  mode: "development",
+  // mode: "development",
   build: {
     minify: false,
   },
-  base:'/dineshigdd.github.io/'
+  base:'/'
 })
