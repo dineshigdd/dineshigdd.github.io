0ed0ebb8e8a3f68943cae9d06b999399a7969e53
unknown <dineshigdd@gmail.com>
2023-03-19 02:18:13 -0700

    change basename

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function PortfolioContainer() {
 
   return (
     <Container fluid>
-       <Router basename={ 'https://dineshigdd.github.io/' }>
+       <Router basename={ 'dineshigdd.github.io/' }>
           <Row className='d-flex flex-column'>     
             <Col>
               <Navbar bg="light" expand="lg" fixed="top">
