d09857f912029a5aad5702c639e407587d7c6545
Dinesh <dineshigdd@gmail.com>
2025-05-07 19:21:29 -0700

    add external links

--- src/views/Portfolio.jsx
+++ src/views/Portfolio.jsx
@@ -35,21 +35,24 @@ function Portfolio() {
     <HomeContainer className="w-50 col-sm-1 w-sm-100 p-3 d-flex mx-auto">   
      <GridContainer  className='grid d-flex flex-column'>
         { projectArray.map( (project, index ) =>(
-
-                  <RowContainer>
-                  <Col><ImageContainer  src={ index }>
-                        <StyledImage src={project.img } /> 
-                  </ImageContainer>                           
+                  <StyledLink href={ project.url } target="_blank">
+                      <RowContainer>
+                          <Col>
                           
-                  </Col>
-                  <Col>
-                  <ProjectDescription> 
-                        <h5>{ project.name }</h5>
-                        <p style={{ textAlign:'left'}}>{ project.description}</p>
-                        <p style={{ textAlign:'left'}}>Technologies:{ project.tech }</p>  
-                  </ProjectDescription>            
-                  </Col>
+                            <ImageContainer  src={ index }>
+                                <StyledImage src={project.img } /> 
+                          </ImageContainer>                           
+                                  
+                          </Col>
+                          <Col>
+                          <ProjectDescription> 
+                                <h5>{ project.name }</h5>
+                                <p style={{ textAlign:'left'}}>{ project.description}</p>
+                                <p style={{ textAlign:'left'}}>Technologies:{ project.tech }</p>  
+                          </ProjectDescription>            
+                          </Col>                  
                   </RowContainer>
+                </StyledLink>
               )
         )                
         }               
@@ -89,6 +92,14 @@ const GridContainer = styled( Container )`
  
   
 `;
+
+const StyledLink = styled.a`
+  text-decoration: none;
+  color: inherit;
+  display: block; /* Make the link fill the RowContainer */
+  width: 100%;
+  height: 100%;
+`
 
 const RowContainer = styled( Row )`
   
