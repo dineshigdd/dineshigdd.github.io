006da3d6f2c223c66a41ec44d16873244edfec5d
Dinesh <dineshigdd@gmail.com>
2025-05-14 00:47:00 -0700

    add code to send email

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import React , { useRef } from 'react'
 import HomeContainer from '../styles/styles'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import styled from 'styled-components';
+import emailjs from '@emailjs/browser'
 
-const handleSubmit =  () =>{
 
-}
 
 function Contact() {
+    const form = useRef();
+
+      const handleSubmit =  () =>{
+        e.preventDefault();
+
+          emailjs
+            .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', {
+              publicKey: 'PvA7-bhOjUMwHZK6b',
+            })
+            .then(
+              () => {
+                console.log('SUCCESS!');
+              },
+              (error) => {
+                console.log('FAILED...', error.text);
+              },
+            );
+
+      }
+   
   return (
     <HomeContainer className="w-50 d-flex justify-content-center mx-auto">
     <div className='inner-container d-flex'>
        
-          <FormContainer onSubmit={ handleSubmit }>
+          <FormContainer ref={form} onSubmit={ handleSubmit }>
               <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                   <Form.Label >Email Address</Form.Label>
                   <Form.Control type="email" placeholder="name@example.com" />
