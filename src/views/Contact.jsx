3961dbfddb8852672c2c91749288603f87a7883a
Dinesh <dineshigdd@gmail.com>
2025-05-21 22:45:44 -0700

    modify constact form

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -10,14 +10,14 @@ import emailjs from '@emailjs/browser'
 function Contact() {
     const formRef = useRef();
 
-      const handleSubmit =  () =>{
-        e.preventDefault();
-
+      const handleSubmit =  (e) =>{
+         e.preventDefault();
+        
          emailjs.sendForm(
               import.meta.env.EMAILJS_SERVICE_ID,
               import.meta.env.EMAILJS_TEMPLATE_ID,
               formRef.current,
-              import.meta.env.EMAILJS_PUBLIC_KEY
+              { publicKey : import.meta.env.EMAILJS_PUBLIC_KEY }
             ) 
             .then(
               () => {
@@ -36,12 +36,12 @@ function Contact() {
        
           <FormContainer ref={ formRef } onSubmit={ handleSubmit }>
               <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                  <Form.Label >Email Address</Form.Label>
-                  <Form.Control type="email" placeholder="name@example.com" />
+                  <Form.Label>Email Address</Form.Label>
+                  <Form.Control name="user_email" type="email" placeholder="name@example.com" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                   <Form.Label>Message</Form.Label>
-                  <Form.Control as="textarea" rows={4} />
+                  <Form.Control name="message" as="textarea" rows={4} />
             </Form.Group>
                 
                 <Button variant="primary" type="submit">
